fc78fff8e72d777f667c518e706e0512b59c4b41
Kris OMealy <kris.omealy@tealium.com>
2020-12-01 15:41:45 -0800

    client upstart is kinda silly anyway, and doesn't work on new Ubuntu

--- recipes/upstart.rb
+++ recipes/upstart.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # Author:: Jennifer Frisk
 # Cookbook Name:: chef-client
@@ -17,37 +19,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+return unless node['lsb']['codename'].eql?('trusty')
 
-	if ::File.executable?(node["chef_client"]["bin"])
-	  client_bin = node["chef_client"]["bin"]
-	# search for the bin in some sane paths
-	elsif Chef::Client.const_defined?('SANE_PATHS') && (chef_in_sane_path=Chef::Client::SANE_PATHS.map{|p| p="#{p}/chef-client";p if ::File.executable?(p)}.compact.first) && chef_in_sane_path
-	  client_bin = chef_in_sane_path
-	# last ditch search for a bin in PATH
-	elsif (chef_in_path=%x{which chef-client}.chomp) && ::File.executable?(chef_in_path)
-	  client_bin = chef_in_path
-	else
-	  raise "Could not locate the chef-client bin in any known path. Please set the proper path by overriding node['chef_client']['bin'] in a role."
-	end
-  
-  if (8.04..9.04).include?(node["platform_version"].to_f)
-    upstart_job_dir = "/etc/event.d"
-    upstart_job_suffix = ""
-  else
-    upstart_job_dir = "/etc/init"
-    upstart_job_suffix = ".conf"
+if ::File.executable?(node['chef_client']['bin'])
+  client_bin = node['chef_client']['bin']
+  # search for the bin in some sane paths
+elsif Chef::Client.const_defined?('SANE_PATHS') && (chef_in_sane_path = Chef::Client::SANE_PATHS.map do |p|
+  p = "#{p}/chef-client"; p if ::File.executable?(p)
+end .compact.first) && chef_in_sane_path
+  client_bin = chef_in_sane_path
+  # last ditch search for a bin in PATH
+elsif (chef_in_path = %x(which chef-client).chomp) && ::File.executable?(chef_in_path)
+  client_bin = chef_in_path
+else
+  raise "Could not locate the chef-client bin in any known path. Please set the proper path by overriding node['chef_client']['bin'] in a role."
   end
 
-  template "#{upstart_job_dir}/chef-client-upstart#{upstart_job_suffix}" do
-    source "debian/init/chef-client-upstart.conf.erb"
-    mode 0644
-    variables(
-      :client_bin => client_bin
-    )
-    notifies :restart, "service[chef-client-upstart]", :delayed
-  end
+if (8.04..9.04).include?(node['platform_version'].to_f)
+  upstart_job_dir = '/etc/event.d'
+  upstart_job_suffix = ''
+else
+  upstart_job_dir = '/etc/init'
+  upstart_job_suffix = '.conf'
+end
 
-  service "chef-client-upstart" do
-    provider Chef::Provider::Service::Upstart
-    action [:enable,:start]
-  end
+template "#{upstart_job_dir}/chef-client-upstart#{upstart_job_suffix}" do
+  source 'debian/init/chef-client-upstart.conf.erb'
+  mode 0o644
+  variables(
+    client_bin: client_bin,
+  )
+  notifies :restart, 'service[chef-client-upstart]', :delayed
+end
+
+service 'chef-client-upstart' do
+  provider Chef::Provider::Service::Upstart
+  action %i[enable start]
+end
